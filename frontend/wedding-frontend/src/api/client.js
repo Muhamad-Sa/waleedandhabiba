abc106438925e9783429a9917bc4d454efa7e610
Muhamad-sa <mohamedsamiir7@gmail.com>
2026-04-05 01:19:41 +0200

    ..

--- frontend/wedding-frontend/src/api/client.js
+++ frontend/wedding-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://waleedandhabiba-backend.vercel.app",
+    baseURL: "https://waleedandhabiba-backend.vercel.app/api",
 });
 
 export default api;
